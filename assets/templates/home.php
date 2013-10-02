cbd010137b003760b95b83fc1e17ae9ce02eedf1
polikin <tumblr@polikin.ca>
2013-10-02 10:32:40 -0700

    initial commit

---
+++ assets/templates/home.php
@@ -0,0 +1,75 @@
+<?php include 'inc.header.php' ?>
+
+<div class="container cf">
+	
+	<h1>
+		Compass
+		<span>http://compass-style.org/reference/compass/css3/</span>
+	</h1>
+	
+	<div class="examples">
+		<div class="primary">
+			<code>$primary</code>
+		</div>
+		
+		<div class="secondary">
+			<code>$secondary</code>
+		</div>
+		
+		<div class="tertiaire">
+			<code>$tertiaire</code>
+		</div>
+		
+		<div class="border-radius">
+			<code>@include border-radius(50px)</code>
+		</div>
+
+		<div class="border-bottom-right-radius">
+			<code>@include border-radius-bottom-right(30px)</code>
+		</div>
+
+		<div class="transition">
+			<code>@include transitions(0.3s)</code>
+		</div>
+	
+		<div class="transition transition-delay">
+			<code>@include transitions-delay(1s)</code>
+		</div>
+
+		<div class="box-shadow">
+			<code>@include box-shadow(10px)</code>
+		</div>
+
+		<div class="opacity">
+			<code>@include opacity(0.6)</code>
+		</div>
+
+		<div class="extend">
+			<code>
+				@extend %placeholder <br>{ border-bottom: 4px blue solid; }
+			</code>
+		</div>
+
+		<div class="extend2">
+			<code>
+				@extend %placeholder <br>{ border-bottom: 4px blue solid; }
+			</code>
+		</div>
+
+		<div class="extend3">
+			<code>
+				@extend %placeholder <br>{ border-bottom: 4px blue solid; }
+			</code>
+		</div>
+		
+		<div class="image">
+			<code>
+				@include background(image-url(loop.svg)
+			</code>
+		</div>
+		
+	</div>
+	
+</div>
+
+<?php include 'inc.footer.php' ?>
---
+++ assets/templates/inc.header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* @copyright (c) Locomotive 2013
+* @author Nicolas Poliquin <nicolas@polikin.ca>
+* @author Mathieu Rivest <rivest.mathieu@gmail.com>
+*/
+
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="gt-ie"> <!--<![endif]-->
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	
+		<title>Sass | Compass</title>
+		
+		<meta name="viewport" content="width=device-width,initial-scale=1; maximum-scale=1; minimum-scale=1; user-scalable=no;"/>
+				
+		<link rel="stylesheet" type="text/css" href="assets/styles/screen.css" media="screen,projection" />
+	
+		<script type="text/javascript" charset="utf-8" src="assets/scripts/jquery-1.10.2.min.js"></script>
+		
+		
+		
+	</head>	
+	<body>
